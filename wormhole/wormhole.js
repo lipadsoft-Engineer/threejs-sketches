af61b1da9ada08fafb86e9e716ccdd8c3ef9ff18
lipadsoft-Engineer <pierreclassique391@gmail.com>
2025-06-02 22:18:06 +0300

    Create a camera and set its position

--- wormhole/wormhole.js
+++ wormhole/wormhole.js
@@ -9,7 +9,13 @@ const h = window.innerHeight;
 renderer.setSize(w, h);
 document.body.appendChild(renderer.domElement);
 
-
+// create a Camera and set its position
+const fov = 75;
+const aspect = w / h;
+const near = 0.1;
+const far = 10;
+const camera = new THREE.PerspectiveCamera(fov, aspect, near, far);
+camera.position.z = 2;
 
 
 
