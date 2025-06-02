d7bca57a1d7b70c26c35e266584620feac0b31c4
lipadsoft-Engineer <pierreclassique391@gmail.com>
2025-06-02 22:17:30 +0300

    Create a renderer and set its size

--- wormhole/wormhole.js
+++ wormhole/wormhole.js
@@ -1,3 +1,16 @@
 // Wormhole imports
 import * as THREE from 'three';
 
+// create a Renderer and set its size
+const renderer = new THREE.WebGLRenderer({ antialias: true });
+
+const w = window.innerWidth;
+const h = window.innerHeight;
+renderer.setSize(w, h);
+document.body.appendChild(renderer.domElement);
+
+
+
+
+
+
