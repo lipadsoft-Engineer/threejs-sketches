9a368d846458d4deac086306e798d83520e8d653
lipadsoft-Engineer <pierreclassique391@gmail.com>
2025-06-04 23:39:04 +0300

    Create a line geometry from spline

--- wormhole/wormhole.js
+++ wormhole/wormhole.js
@@ -1,6 +1,8 @@
 // Wormhole imports
 import * as THREE from 'three';
 import { OrbitControls } from 'three/examples/jsm/Addons.js';
+import spline from './spline';
+
 
 // create a Renderer and set its size
 const renderer = new THREE.WebGLRenderer({ antialias: true });
@@ -9,6 +11,9 @@ const w = window.innerWidth;
 const h = window.innerHeight;
 renderer.setSize(w, h);
 document.body.appendChild(renderer.domElement);
+
+// create a Scene
+const scene = new THREE.Scene();
 
 // create a Camera and set its position
 const fov = 75;
@@ -23,8 +28,16 @@ const controls = new OrbitControls(camera, renderer.domElement);
 controls.enableDamping = true;
 controls.dampingFactor = 0.03;
 
-// create a Scene
-const scene = new THREE.Scene();
+// create a line geometry from the spline
+const points = spline.getPoints(100);
+const geo = new THREE.BufferGeometry().setFromPoints(points);
+const mat = new THREE.LineBasicMaterial({
+    color: 0x00ff00
+});
+const line = new THREE.Line(geo, mat);
+scene.add(line);
+
+
 
 // Define a function to animate and render
 function animate(){
