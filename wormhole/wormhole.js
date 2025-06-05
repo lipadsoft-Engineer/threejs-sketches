201b2b0f8ad4458c63497d4831931cebbad88507
lipadsoft-Engineer <pierreclassique391@gmail.com>
2025-06-05 15:03:15 +0300

    Define fn for updating camera

--- wormhole/wormhole.js
+++ wormhole/wormhole.js
@@ -54,13 +54,24 @@ const hemiLight = new THREE.HemisphereLight(0xffffff, 0x444444);
 scene.add(hemiLight);
 
 
-// function to update camera for fly thru animation
+// update camera for fly thru animation
+function updateCamera(t){
+    const time = t * 0.0005;
+    const loopTime = 20 * 1000;
+    const point = ( time % loopTime) / loopTime;
+    const pos = tubeGeo.parameters.path.getPointAt(point);
+    const lookAt = tubeGeo.parameters.path.getPointAt((point + 0.03) % 1);
+    camera.position.copy(pos);
+    camera.lookAt(lookAt);
+}
 
 
 // function to animate and render
-function animate(){
+function animate(t = 0){
     requestAnimationFrame(animate);
     renderer.render(scene, camera);
+    updateCamera(t);
+    control.update();
 }
 animate();
 
