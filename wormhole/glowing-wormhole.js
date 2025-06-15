eaf61cb317589438d51f8d0ca70e1424c607f471
lipadsoft-Engineer <pierreclassique391@gmail.com>
2025-06-15 15:07:55 +0300

    Change import from skypak to ES6 module CDN

--- wormhole/glowing-wormhole.js
+++ wormhole/glowing-wormhole.js
@@ -1,4 +1,4 @@
-import * as THREE from 'https://cdn.skypack.dev/three@0.160.0';
+import * as THREE from 'https://unpkg.com/three@0.160.0/build/three.module.js';
 import { OrbitControls } from 'https://cdn.jsdelivr.net/npm/three@0.160.0/examples/jsm/controls/OrbitControls.js';
 import { EffectComposer } from 'https://cdn.jsdelivr.net/npm/three@0.160.0/examples/jsm/postprocessing/EffectComposer.js';
 import { RenderPass } from 'https://cdn.jsdelivr.net/npm/three@0.160.0/examples/jsm/postprocessing/RenderPass.js';
