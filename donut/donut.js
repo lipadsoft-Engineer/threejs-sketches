87a120d3012eccdf58f5739ba11272e66c58ebaa
lipadsoft-Engineer <pierreclassique391@gmail.com>
2025-07-31 09:49:42 +0300

    Create a scene, camera and renderer

--- donut/donut.js
+++ donut/donut.js
@@ -1,2 +1,14 @@
  import * as THREE from 'https://unpkg.com/three@0.160.0/build/three.module.js';
     import { OrbitControls } from 'https://unpkg.com/three@0.160.0/examples/jsm/controls/OrbitControls.js';
+
+    // scene
+    const scene = new THREE.Scene();
+
+    // Camera
+    const camera = new THREE.PerspectiveCamera(75, window.innerWidth/window.innerHeight, 0.1, 100);
+    camera.position.z = 5;
+
+    // Renderer
+    const renderer = new THREE.WebGLRenderer({ antialias: true });
+    renderer.setSize(window.innerWidth, window.innerHeight);
+    document.body.appendChild(renderer.domElement);
