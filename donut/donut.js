449f8e4fef3834353994c1e93363e8e4bb26d70e
lipadsoft-Engineer <pierreclassique391@gmail.com>
2025-07-31 09:50:52 +0300

    Add lights

--- donut/donut.js
+++ donut/donut.js
@@ -12,3 +12,10 @@
     const renderer = new THREE.WebGLRenderer({ antialias: true });
     renderer.setSize(window.innerWidth, window.innerHeight);
     document.body.appendChild(renderer.domElement);
+
+    // Lights
+    const ambientLight = new THREE.AmbientLight(0xffffff, 0.5);
+    scene.add(ambientLight);
+    const pointLight = new THREE.PointLight(0xffffff, 1);
+    pointLight.position.set(5, 5, 5);
+    scene.add(pointLight);
