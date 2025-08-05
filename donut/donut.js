829aad109db5d5990fc6622210f52348e3a1fff6
lipadsoft-Engineer <pierreclassique391@gmail.com>
2025-08-05 12:49:05 +0300

    Change three import to remove shorthand three

--- donut/donut.js
+++ donut/donut.js
@@ -1,5 +1,7 @@
- import * as THREE from 'https://unpkg.com/three@0.160.0/build/three.module.js';
-    import { OrbitControls } from 'https://unpkg.com/three@0.160.0/examples/jsm/controls/OrbitControls.js';
+
+    import * as THREE from 'https://unpkg.com/three@0.160.0/build/three.module.js';
+    import { OrbitControls } from 'https://cdn.jsdelivr.net/npm/three@0.160.0/examples/jsm/controls/OrbitControls.js';
+
 
     // scene
     const scene = new THREE.Scene();
